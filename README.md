0d453390b193e02c9a27a5605611e033e5eba79a
Mario Ramosa <marioramosa06@gmail.com>
2024-10-25 15:57:35 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+Simple Video Player (JavaScript)
+This project is a simple video player built using JavaScript. It allows you to perform basic video playback controls like play, pause, next, previous, and select multiple files.
+
+Features
+Play/Pause: Start or pause the currently playing video.
+Next/Previous: Switch to the next or previous video in the playlist.
+Select Multiple Files: Choose multiple videos to create a playlist.
+
+Usage
+
+1. Clone the Repository:
+git clone https://github.com/marioramosa06/simple-video-player.git
+
+2. Run : 
+Open the index.html file in your web browser.
---
+++ js/script.js
@@ -0,0 +1,140 @@
+const htmlTitle = document.getElementById("title");
+const filePicker = document.querySelector("#file");
+const video = document.querySelector("#video");
+const controls = document.querySelector(".controls");
+const playBtn = document.getElementById("play-btn");
+const playBtnIcon = document.getElementById("play-btn-icon");
+const prevBtn = document.getElementById("prev-btn");
+const nextBtn = document.getElementById("next-btn");
+const slider = document.getElementById("slider");
+const elapsedTime = document.getElementById("time-elapsed");
+const totalTime = document.getElementById("time-total")
+
+let playlist = [];
+let videoIndex = 0;
+
+function changePlayState() {
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+function timeFormat(t) {
+    let min = Math.floor(t / 60)
+    let sec = Math.floor(t - (min * 60))
+    let time = `${min > 9 ? min : "0" + min} : ${sec > 9 ? sec : "0" + sec}`
+    return time
+}
+
+function loadVideo() {
+    let file = new FileReader();
+    file.readAsDataURL(playlist[videoIndex]);
+    file.onload = () => {
+        video.setAttribute("src", file.result);
+    };
+
+    let videoDuration = 0
+    video.onloadedmetadata = function () {
+        console.log('metadata loaded!');
+        console.log(this.duration); //this refers to myVideo
+        videoDuration = this.duration
+        totalTime.innerText = timeFormat(videoDuration)
+        slider.max = Math.floor(videoDuration);
+    };
+
+    if (file.DONE) {
+        htmlTitle.innerText = playlist[videoIndex].name;
+        window.setInterval(function () {
+            slider.value = Math.floor(video.currentTime);
+            // totalTime.innerText = timeFormat(videoDuration - video.currentTime)
+            elapsedTime.innerText = timeFormat(video.currentTime)
+            if (video.ended) {
+                nextFile()
+            }
+        }, 1000);
+    }
+}
+
+function prevFile() {
+    if (videoIndex > 0) {
+        videoIndex--;
+        loadVideo();
+    }
+}
+
+function nextFile() {
+    if (videoIndex < playlist.length - 1) {
+        videoIndex++;
+        loadVideo();
+    }
+}
+
+window.addEventListener("keydown", function (e) {
+    e.preventDefault;
+    console.log(e);
+    if (e.code == "Space") {
+        changePlayState();
+    }
+    if (e.code == "ArrowLeft") {
+        if (video.currentTime > 5) {
+            video.currentTime = video.currentTime - 5
+        }
+    }
+    if (e.code == "ArrowRight") {
+        if (video.currentTime < video.duration - 5) {
+            video.currentTime = video.currentTime + 5
+        }
+    }
+});
+
+let controlsVisivilityTimeOut = null;
+
+window.addEventListener("mousemove", function () {
+    if (controlsVisivilityTimeOut !== null) {
+        window.clearTimeout(controlsVisivilityTimeOut);
+    }
+    if (controls.classList.contains("hide")) {
+        controls.classList.remove("hide");
+    }
+    controlsVisivilityTimeOut = this.setTimeout(() => {
+        controls.classList.add("hide");
+        controlsVisivilityTimeOut = null;
+    }, 3000);
+});
+
+
+filePicker.addEventListener("change", function () {
+    playlist = filePicker.files;
+    loadVideo();
+});
+
+playBtn.addEventListener("click", function () {
+    changePlayState();
+});
+
+prevBtn.addEventListener("click", function () {
+    prevFile();
+});
+
+nextBtn.addEventListener("click", function () {
+    nextFile();
+});
+
+slider.addEventListener("change", function () {
+    video.currentTime = slider.value
+})
+slider.addEventListener("input", function () {
+    video.currentTime = slider.value
+})
+
+//change play button state
+video.addEventListener("play", function () {
+    playBtnIcon.classList.remove("ri-play-fill")
+    playBtnIcon.classList.add("ri-pause-fill")
+});
+video.addEventListener("pause", function () {
+    playBtnIcon.classList.remove("ri-pause-fill")
+    playBtnIcon.classList.add("ri-play-fill")
+});
